93c7723791778a50eddb6e2eb909b343e3244fce
Aaron Rietschlin <nemisis.82@gmail.com>
2013-11-25 17:36:36 -0500

    Added some more stuff to get the bluetooth working.

--- App/src/main/java/com/duethealth/test/ble/MainActivity.java
+++ App/src/main/java/com/duethealth/test/ble/MainActivity.java
@@ -3,15 +3,22 @@ package com.duethealth.test.ble;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
+import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -19,6 +26,7 @@ import com.duethealth.lib.component.DhBaseAdapter;
 import com.google.android.gms.internal.p;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.InjectView;
 import butterknife.Views;
@@ -66,7 +74,71 @@ public class MainActivity extends Activity {
         super.onPostCreate(savedInstanceState);
         mListAdapter = new BtArrayAdapter(this);
         mList.setAdapter(mListAdapter);
+
+        mList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                BluetoothDevice device = (BluetoothDevice) mListAdapter.getItem(position);
+                connectGATT(device);
+            }
+        });
     }
+
+    private void connectGATT(BluetoothDevice device){
+        device.connectGatt(this, false, mGattCallback);
+    }
+
+    private BluetoothGattCallback mGattCallback  = new BluetoothGattCallback() {
+        @Override
+        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+            super.onConnectionStateChange(gatt, status, newState);
+            gatt.discoverServices();
+            List<BluetoothGattService> services = gatt.getServices();
+            Log.d("", "");
+        }
+
+        @Override
+        public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+            super.onServicesDiscovered(gatt, status);
+            List<BluetoothGattService> services = gatt.getServices();
+            Log.d("", "");
+        }
+
+        @Override
+        public void onCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+            super.onCharacteristicRead(gatt, characteristic, status);
+        }
+
+        @Override
+        public void onCharacteristicWrite(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+            super.onCharacteristicWrite(gatt, characteristic, status);
+        }
+
+        @Override
+        public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
+            super.onCharacteristicChanged(gatt, characteristic);
+        }
+
+        @Override
+        public void onDescriptorRead(BluetoothGatt gatt, BluetoothGattDescriptor descriptor, int status) {
+            super.onDescriptorRead(gatt, descriptor, status);
+        }
+
+        @Override
+        public void onDescriptorWrite(BluetoothGatt gatt, BluetoothGattDescriptor descriptor, int status) {
+            super.onDescriptorWrite(gatt, descriptor, status);
+        }
+
+        @Override
+        public void onReliableWriteCompleted(BluetoothGatt gatt, int status) {
+            super.onReliableWriteCompleted(gatt, status);
+        }
+
+        @Override
+        public void onReadRemoteRssi(BluetoothGatt gatt, int rssi, int status) {
+            super.onReadRemoteRssi(gatt, rssi, status);
+        }
+    };
 
     private void scaneBle(final boolean enable) {
         if (enable) {
